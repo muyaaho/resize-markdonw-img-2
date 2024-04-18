02af60e8cd059375b3b2fa9e780bb2388cb3f79b
muyaaho <skanthsu715@gmail.com>
2024-04-18 20:59:06 +0900

    Feat: init files

---
+++ src/main/java/com/example/resizeimage2/ResizeImage2Application.java
@@ -0,0 +1,13 @@
+package com.example.resizeimage2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResizeImage2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResizeImage2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/resizeimage2/ResizeImage2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.resizeimage2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResizeImage2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
