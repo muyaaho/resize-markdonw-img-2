883cee68329098f302f97516ea5c149724d524bb
muyaaho <skanthsu715@gmail.com>
2024-05-24 17:33:29 +0900

    Feat: 폼 데이터 전송 기능

--- src/main/java/com/example/resizeimage2/controller/Controller.java
+++ src/main/java/com/example/resizeimage2/controller/Controller.java
@@ -1,6 +1,7 @@
 package com.example.resizeimage2.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @org.springframework.stereotype.Controller
 public class Controller {
@@ -8,5 +9,10 @@ public class Controller {
     @GetMapping("/new")
     public String newForm() {
         return "new";
+    }
+
+    @PostMapping("/modify")
+    public String Form() {
+        return "";
     }
 }
